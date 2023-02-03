17568b26be893a0b0159a560441063869d73d957
igorro79 <griniov.igor@gmail.com>
2023-02-03 16:16:50 +0200

    main and footer markup done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,34 +1,31 @@
-// const btn = document.querySelector('.burger');
-// const navBtn = document.querySelector('.header');
-// const body = document.querySelector('body');
-// const main = document.querySelector('.main');
-const allServices = document.querySelector(".services__cards-wrapper");
+const servicesList = document.querySelector(".services__cards-list");
+const faqList = document.querySelector(".faq__list");
+const faqQuestionList = document.querySelectorAll(".faq__question");
+const faqButtonList = document.querySelectorAll(".faq__question-button");
+const faqAnsvers = document.querySelectorAll(".faq__answer");
 
-allServices.addEventListener("click", (e) => {
+servicesList.addEventListener("click", (e) => {
   if (e.target.className === "services__card-title") {
-    e.target.parentNode.parentNode.lastElementChild.classList.toggle("open");
+    e.target.parentNode.parentNode.lastElementChild.classList.add("open");
   }
 });
-// btn.addEventListener('click', () => {
-//     btn.classList.toggle('is-active');
-//     navBtn.classList.toggle('header--active');
-//     body.classList.toggle('body-bg');
-//     main.classList.toggle('main--active');
-//     linkList.style.left = '0';
-// });
 
-// const linkServices = document.querySelector('.nav__link-services');
-// const linkList = document.querySelector('.link__list');
-// const navItem = document.querySelector('.nav__item');
+faqList.addEventListener("click", (e) => {
+  if (e.target.className === "faq__question-button") {
+    faqQuestionList.forEach((i) => {
+      if (i.classList.contains("faq__question--active"))
+        i.classList.remove("faq__question--active");
+    });
+    faqButtonList.forEach((i) => {
+      if (i.classList.contains("faq__question-button--active"))
+        i.classList.remove("faq__question-button--active");
+    });
+    faqAnsvers.forEach((i) => {
+      if (i.classList.contains("open")) i.classList.remove("open");
+    });
 
-// linkServices.addEventListener('click', () => {
-//     navItem.style.left = '100%';
-//     linkList.setAttribute('style', 'left: 0; transform: scale(1);');
-// });
-
-// const linkListBtn = document.querySelector('.link__list-btn');
-
-// linkListBtn.addEventListener('click', () => {
-//     navItem.style.left = '0';
-//     linkList.setAttribute('style', 'left: 100%; transform: scale(0);');
-// });
+    e.target.parentNode.parentNode.lastElementChild.classList.add("open");
+    e.target.classList.add("faq__question-button--active");
+    e.target.previousElementSibling.classList.add("faq__question--active");
+  }
+});
