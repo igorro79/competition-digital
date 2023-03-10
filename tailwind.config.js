a24efbf18847a20ce1e2b6515d7b7627fb3574c8
igorro79 <griniov.igor@gmail.com>
2023-03-10 18:58:58 +0200

    404 done

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,20 @@ module.exports = {
         bgColorBtnForm: "#535353",
         mainTextBlue: "#6355ff",
       },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
     },
   },
   corePlugins: {
