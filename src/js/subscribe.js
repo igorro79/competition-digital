3aae693760786f90a64ed10712947f95663f2dc6
igorro79 <griniov.igor@gmail.com>
2023-03-14 03:00:19 +0200

    add subscribe fn

--- src/js/subscribe.js
+++ src/js/subscribe.js
@@ -1,29 +1,42 @@
-id = "subscribe";
-const modal = document.getElementById("modal");
-const subscribeInput = document.getElementById("subscribe");
 const subscribeBtn = document.getElementById("subscribeBtn");
+const handleSubscribeBtn = document.getElementById("handleSubscribeBtn");
+const modal = document.getElementById("modal");
+const subscribeInput = document.getElementById("subscribeInput");
 const closeModal = document.querySelector(".modal-close-btn");
 
-openVideoBtn.addEventListener("click", (e) => playVideo(e.target));
+subscribeBtn.addEventListener("click", openSubscribe);
 
-function closeModal() {
-  unlockWrapper();
+function closeModalEsc(e) {
+  console.log(e.key);
+  if (e.key === "Escape") {
+    closeSubscribe();
+  }
+}
+function closeSubscribe() {
+  document.removeEventListener("keydown", closeModalEsc);
+
+  closeModal.removeEventListener("click", closeSubscribe);
   modal.classList.toggle("visible");
   modal.classList.toggle("invisible");
   modal.classList.toggle("opacity-0");
 
-  modal.querySelector("#video").setAttribute("src", "");
-  modal
-    .querySelector(".modal-close-btn")
-    .removeEventListener("click", closeModal);
+  unlockWrapper();
 }
 
-function openModal() {
-  videoModal
-    .querySelector(".modal-close-btn")
-    .addEventListener("click", closeModal);
-  lockWrapper();
+function openSubscribe() {
+  document.addEventListener("keydown", closeModalEsc);
+  closeModal.addEventListener("click", closeSubscribe);
+  handleSubscribeBtn.addEventListener("click", handleSubscribe);
   modal.classList.toggle("visible");
   modal.classList.toggle("invisible");
   modal.classList.toggle("opacity-0");
+  lockWrapper();
+}
+
+function handleSubscribe() {
+  if (checkEmail(subscribeInput)) {
+    console.log(subscribeInput.value);
+  } else {
+    console.log("object");
+  }
 }
